21f4d351e989357dce22b1fdab80ac37b8da4c35
annamjdy <57040076+annamjdy@users.noreply.github.com>
2019-11-16 15:44:26 +0100

    Update script.js
    adding input

--- js/script.js
+++ js/script.js
@@ -32,5 +32,14 @@ function newElement() {
     divElementText.appendChild(editInput);
     divElementText.appendChild(iClose);
   
-  
+  ////////adds input
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    itemText.appendChild(t);
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        document.getElementById("myUL").appendChild(li);
+    };
+    document.getElementById("myInput").value = "";
 }
