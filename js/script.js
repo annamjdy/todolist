2e02452d220430cf0519b4e0f6652c082e134ffc
annamjdy <57040076+annamjdy@users.noreply.github.com>
2019-11-16 16:07:27 +0100

    Update script.js
    editTask function, date of the edited task

--- js/script.js
+++ js/script.js
@@ -53,6 +53,35 @@ function newElement() {
         li.parentNode.removeChild(li);
     })
     
-   
+    ///////// edit task
+
+    var buttonEdit = document.createElement('button');
+    buttonEdit.classList.add('edit');
+    buttonEdit.textContent = "edit";
+    divElementText.appendChild(buttonEdit);
+
+
+    var editTask = function () {
+        var divElementText = this.parentNode;
+        var editInput = divElementText.querySelector('input[type=text]');
+        var containsClass = divElementText.classList.contains("editMode");
+        //If class of the parent is .editmode
+        if (containsClass && itemText.innerText != editInput.value) {
+            //switch to .editmode
+            //label becomes the inputs value.
+            itemText.innerText = editInput.value;
+            // date of the edited task
+            var editDate = new Date();
+            const modifyDate = dateText + ", modified: " + editDate.getDate() + '-' + (editDate.getMonth() + 1) + '-' + editDate.getFullYear() + ', ' + editDate.getHours() + ':' + (editDate.getMinutes() < 10 ? '0' : '') + editDate.getMinutes();
+            divDate.innerText = modifyDate;
+        } else {
+            editInput.value = itemText.innerText;
+        }
+
+        //toggle .editmode on the parent.
+        divElementText.classList.toggle("editMode");
+    }
+
+    buttonEdit.addEventListener('click', editTask)
     
 }
